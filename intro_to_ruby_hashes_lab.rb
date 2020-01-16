88b3843431eb0ab2d665ff1de3c2f30429480d89
Justin Bermudez <justinanbermudez@gmail.com>
2020-01-16 02:59:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,8 @@ def monopoly_with_third_tier
 	monopoly = {
 		:railroads => {
 			:pieces => 4,
-			:rent_in_dollars => :names => {
+			:rent_in_dollars =>
+			:names => {
 				:name1 => "",
 				:name2 => "",
 				:name3 => "",
